bff267fc6218c3262701bc72c96bb292013e09c3
Luís Ferreira <contact@lsferreira.net>
2020-02-23 16:44:30 +0000

    headers: add missing LICENSE headers
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ header_GENERIC.d
@@ -0,0 +1,25 @@
+/*
+                                    __
+                                   / _|
+  __ _ _   _ _ __ ___  _ __ __ _  | |_ ___  ___ ___
+ / _` | | | | '__/ _ \| '__/ _` | |  _/ _ \/ __/ __|
+| (_| | |_| | | | (_) | | | (_| | | || (_) \__ \__ \
+ \__,_|\__,_|_|  \___/|_|  \__,_| |_| \___/|___/___/
+
+Copyright (C) 2018-2020 Aurora Free Open Source Software.
+
+This file is part of the Aurora Free Open Source Software. This
+organization promote free and open source software that you can
+redistribute and/or modify under the terms of the license available in the
+package root path as 'LICENSE' file. Please review the following
+information to ensure that the license requirements meet the opensource
+guidelines at opensource.org .
+
+NOTE: All products, services or anything associated to trademarks and
+service marks used or referenced on this file are the property of their
+respective companies/owners or its subsidiaries. Other names and brands
+may be claimed as the property of others.
+
+For more info about intellectual property visit: aurorafoss.org or
+directly send an email to: contact (at) aurorafoss.org .
+*/
